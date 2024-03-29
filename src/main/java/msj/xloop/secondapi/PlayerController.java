7447427ac82b0430239a3e97d2201e9e9dc17167
webspot <webspot17530@gmail.com>
2024-03-29 12:31:52 +0500

    Get player by id

--- src/main/java/msj/xloop/secondapi/PlayerController.java
+++ src/main/java/msj/xloop/secondapi/PlayerController.java
@@ -1,11 +1,14 @@
 package msj.xloop.secondapi;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,4 +31,13 @@ public class PlayerController {
         repo.save(p);
         return "Played Saved..";
     }
+
+    @GetMapping("/pid")
+    @ResponseBody
+    public Collection<Player> getPlayerData(@RequestParam int id) {
+        
+        return repo.getAll().stream().filter(a-> a.getId() == id).collect(Collectors.toList());
+    }
+
+    
 }
